07cfffcd77630b63dc73a2a026f3eb1797fbe1ab
KuaBK <tuanphonglqd@gmail.com>
2026-05-05 20:44:24 +0700

    refactor

--- src/main/java/com/capstone/inventoryservice/domain/dto/response/BookingSessionData.java
+++ src/main/java/com/capstone/inventoryservice/domain/dto/response/BookingSessionData.java
@@ -4,16 +4,15 @@ import com.capstone.inventoryservice.domain.dto.request.ReserveRequest;
 import com.capstone.inventoryservice.model.entity.TicketType;
 import lombok.Data;
 
-import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Locale;
 
 @Data
 public class BookingSessionData {
     private Long userId;
     private List<BookingItem> items;
     private String eventName;
-    private String time;
+    private LocalDateTime time;
     private String venue;
 
     @Data
@@ -36,11 +35,7 @@ public class BookingSessionData {
 
         data.setEventName(ticketType.getShowtime().getEvent().getEventName());
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(
-                "HH:mm - EEEE, dd/MM/yyyy",
-                Locale.forLanguageTag("vi-VN")
-        );
-        data.setTime(ticketType.getShowtime().getStartDatetime().format(formatter));
+        data.setTime(ticketType.getShowtime().getStartDatetime());
 
         data.setVenue(ticketType.getShowtime().getFullAddress());
 
