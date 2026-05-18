6512d223f4e0554fb4be11314bb301c55cc6f829
TGG-Henry <tuanphonglqd@gmail.com>
2026-05-18 16:15:13 +0700

    feat: configure RestClient bean with JdkClientHttpRequestFactory

--- src/main/java/com/capstone/inventoryservice/config/RestClientConfig.java
+++ src/main/java/com/capstone/inventoryservice/config/RestClientConfig.java
@@ -3,12 +3,15 @@ package com.capstone.inventoryservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
 
 @Configuration
 public class RestClientConfig {
 
     @Bean
     public RestClient restClient() {
-        return RestClient.builder().build();
+        return RestClient.builder()
+                .requestFactory(new JdkClientHttpRequestFactory())
+                .build();
     }
 }
